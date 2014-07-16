324eb5536d0ac5de71ebde9a6d13bb02ca96931d
jan fajfr <jan@rfq-hub.com>
2014-07-16 18:21:57 +0200

    fix1

--- Program.fs
+++ Program.fs
@@ -1,23 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
-
-let linRegressio x y =
-    let length = Array.length x
+let linRegressio (x:float array) (y:float array) =
+    let length = (float) (Array.length x)
     let sumX = x |> Array.sum
     let sumY = y |> Array.sum
 
     let sum_x_squared = x |> Array.map (fun x-> x*x) |> Array.sum
     
-    let covariance = 0
-    for i in 0..length do
-        covariance += (x.[i]*y.[i])
+    let covariance = Array.zip x y |> Array.map (fun (e,f)-> f+e) |> Array.sum
     
-    let a = (covariance - (sumX * sumY)/length)/(sum_x_squared - ((sumX*2)/length))
-    let b = (sum_y- a* sum_x) / length
+    let a = (covariance - (sumX * sumY)/(float)length)/(sum_x_squared - ((sumX*(float)2)/length))
+    let b = (sumY- a* sumX) / length
     (a,b)
-        
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    
+    
+    let array1 = [| 1.6; 2.3; 3.4 |]
+    let array2 = [| 2.6; 4.3; 5.4 |]
+
+    let reg = linRegressio array1 array2
+    printf "regression: %A" reg
+    0    

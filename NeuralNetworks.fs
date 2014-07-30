8a40665aad3712dd47f130b07e9edfc69f9dd28e
jan fajfr <jan@rfq-hub.com>
2014-07-30 19:03:37 +0200

    adding NN begining

---
+++ NeuralNetworks.fs
@@ -0,0 +1,11 @@
+﻿module MachineLearning.NeuralNetworks
+
+let applyLayer (input:array<int>) (hiddenWeights:(int*int)list list) activation =
+    let length = hiddenWeights |> List.length
+    let output = Array.zeroCreate length
+    hiddenWeights |> List.iteri (fun i weights -> 
+        let sum = weights |> List.sumBy (fun (inputEl,weight) -> input.[inputEl] * weight)
+        output.[i] <- activation sum
+        ()
+    )
+    output
---
+++ NeuralNetworksTests.fs
@@ -0,0 +1,36 @@
+﻿module MachineLearning.NeuralNetworksTests
+
+open MachineLearning.NeuralNetworks
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``test nn hidden test 1``() =
+    let weights = [
+                    [(0,1);(1,1)]
+                    [(1,1);(2,1)]
+                  ]
+    let input = [|1;1;1|]
+
+    let actFunc x = x
+
+    let result = applyLayer input weights actFunc
+    result |> should haveLength 2
+    result.[0] |> should equal 2
+    result.[1] |> should equal 2
+
+[<Test>]
+let ``test nn hidden test 2``() =
+    let weights = [
+                    [(0,3);(1,1)]
+                    [(1,1);(2,1)]
+                  ]
+    let input = [|2;2;1|]
+
+    let actFunc x = x
+
+    let result = applyLayer input weights actFunc
+    result |> should haveLength 2
+    result.[0] |> should equal 8
+    result.[1] |> should equal 3

b787c26369e5143ed6ef0f86936c3a1584a7dce0
jan fajfr <jan@rfq-hub.com>
2014-07-30 15:16:04 +0200

    adding a function to pass a layer of nn

--- Program.fs
+++ Program.fs
@@ -1,5 +1,6 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
+module MachineLearning.Program
 
 let linRegressio (x:float array) (y:float array) =
     let length = (float) (Array.length x)
@@ -14,13 +15,21 @@ let linRegressio (x:float array) (y:float array) =
     let b = (sumY- a* sumX) / length
     (a,b)
 
+
+
+    
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
     
+    let sigmondActivation x =
+        1 / (1 + x * x)
     
     let array1 = [| 1.6; 2.3; 3.4 |]
     let array2 = [| 2.6; 4.3; 5.4 |]
+
+    let weights = [[(1,2),(1,3)]]
+
 
     let reg = linRegressio array1 array2
     printf "regression: %A" reg

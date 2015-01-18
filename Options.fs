78e27033c45ffaf07d5416fbcca5431a58af8d28
Jan Fajfr <hoonzis@gmail.com>
2015-01-18 22:44:12 +0100

    Adding option charting

---
+++ Options.fs
@@ -0,0 +1,37 @@
+﻿module MachineLearning.Options
+
+open System
+open System.Drawing
+open FSharp.Charting
+open FSharp.Charting.ChartTypes
+open System.Windows.Forms
+open System.Windows.Forms.DataVisualization.Charting
+
+
+let euroCallValue strike premium buySell ref = 
+    buySell * (max (ref - strike) 0.0) - (buySell* premium)
+
+let optionPayOff option = 
+    [for p in 0.0 .. 10.0 .. 80.0 -> p, option p]
+
+let buyingCall = optionPayOff (euroCallValue 30.0 5.0 1.0)
+
+let sellingCall = optionPayOff (euroCallValue 30.0 5.0 -1.0)
+    
+let drawPayOff = 
+    let chart = Chart.Combine [         
+            Chart.Line (buyingCall, Name = "buying call")
+            Chart.Line (sellingCall, Name = "selling call")
+        ]
+    let withLegend = chart |> Chart.WithLegend(true)
+    let area = new ChartArea("Main")
+
+    let control = new ChartControl(withLegend)
+    control.Width <- 700
+    control.Height  <- 500
+
+    // Show the chart control on a top-most form
+    let mainForm = new Form(Visible = true, TopMost = true, 
+                            Width = 700, Height = 500)
+    mainForm.Controls.Add(control)
+    mainForm
--- Program.fs
+++ Program.fs
@@ -1,6 +1,6 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-module MachineLearning.Program
+﻿module MachineLearning.Program
+open System.Windows.Forms
+open MachineLearning.Options
 
 let linRegressio (x:float array) (y:float array) =
     let length = (float) (Array.length x)
@@ -14,23 +14,9 @@ let linRegressio (x:float array) (y:float array) =
     let a = (covariance - (sumX * sumY)/(float)length)/(sum_x_squared - ((sumX*(float)2)/length))
     let b = (sumY- a* sumX) / length
     (a,b)
-
-
-
     
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
-    
-    let sigmondActivation x =
-        1 / (1 + x * x)
-    
-    let array1 = [| 1.6; 2.3; 3.4 |]
-    let array2 = [| 2.6; 4.3; 5.4 |]
-
-    let weights = [[(1,2),(1,3)]]
-
-
-    let reg = linRegressio array1 array2
-    printf "regression: %A" reg
-    0    
+    Application.Run(drawPayOff)
+    0
